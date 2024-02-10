e11ccf6790f97e4191455b0eb15eb697b5e1a644
Leo Shaxb <shaxbazyan001@mail.ru>
2024-02-10 10:02:26 +0400

    Add swiftFormat swiftLint

---
+++ UIKitDZ/UIKitDZ/SceneDelegate.swift
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.swift
+//  UIKitDZ
+//
+//  Created by Levon Shaxbazyan on 10.02.24.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+    var window: UIWindow?
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession,
+               options connectionOptions: UIScene.ConnectionOptions) {
+        guard (scene as? UIWindowScene) != nil else { return }
+    }
+}
---
+++ UIKitDZ/UIKitDZ/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  UIKitDZ
+//
+//  Created by Levon Shaxbazyan on 10.02.24.
+//
+
+import UIKit
+
+/// Стартовый вью контроллер
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+    }
+}
