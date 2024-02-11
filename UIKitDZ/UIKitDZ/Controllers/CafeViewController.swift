e6d800c0ed46b0db8cc676ee16c7dcc5795c92c3
Leo Shaxb <shaxbazyan001@mail.ru>
2024-02-11 15:51:43 +0400

    add UI for CafeViewController

---
+++ UIKitDZ/UIKitDZ/Controllers/CafeViewController.swift
@@ -0,0 +1,15 @@
+//
+//  CafeViewController.swift
+//  UIKitDZ
+//
+//  Created by Levon Shaxbazyan on 11.02.24.
+//
+
+import UIKit
+/// Экран кафе
+class CafeViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+    }
+}
