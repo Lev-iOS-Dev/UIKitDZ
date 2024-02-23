afb9d6d9d414cb7767e51f84598927f316b85dc0
Leo Shaxb <shaxbazyan001@mail.ru>
2024-02-23 12:39:40 +0400

    Finish ProfileViewController

--- UIKitDZ/UIKitDZ/Application/SceneDelegate.swift
+++ UIKitDZ/UIKitDZ/Application/SceneDelegate.swift
@@ -5,49 +5,18 @@ import UIKit
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     var window: UIWindow?
+
     func scene(
-        _ scene: UIScene,
-        willConnectTo session: UISceneSession,
-        options connectionOptions: UIScene.ConnectionOptions
+        _ scene: UIScene, willConnectTo _: UISceneSession, options _: UIScene.ConnectionOptions
     ) {
+        setupWindow(scene)
+    }
+
+    private func setupWindow(_ scene: UIScene) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
-        let window = UIWindow(windowScene: windowScene)
-
-        let firstVC = UINavigationController(
-            rootViewController: NewsLineViewController()
-        )
-        let secondVC = UINavigationController(
-            rootViewController: NotificationsViewController()
-        )
-        let thirdVC = UINavigationController(
-            rootViewController: ProfileViewController()
-        )
-
-        firstVC.tabBarItem = UITabBarItem(
-            title: "Лента",
-            image: UIImage(named: "newslineIcon"),
-            tag: 0
-        )
-        secondVC.tabBarItem = UITabBarItem(
-            title: "Уведомления",
-            image: UIImage(named: "notificationIcon"),
-            tag: 1
-        )
-        thirdVC.tabBarItem = UITabBarItem(
-            title: "Профиль",
-            image: UIImage(named: "profileIcon"),
-            tag: 2
-        )
-
-        let tabBarController = UITabBarController()
-        tabBarController.viewControllers = [firstVC, secondVC, thirdVC]
-        tabBarController.tabBar.backgroundColor = .white
-        let navController = UINavigationController(
-            rootViewController: tabBarController
-        )
-
-        window.rootViewController = navController
-        window.makeKeyAndVisible()
-        self.window = window
+        window = UIWindow(windowScene: windowScene)
+        let rootVC = TabBarController()
+        window?.rootViewController = rootVC
+        window?.makeKeyAndVisible()
     }
 }
