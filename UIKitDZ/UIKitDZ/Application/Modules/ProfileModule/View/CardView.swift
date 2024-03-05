bcc37040a93450a1a4ecb16eaffa7a7735c38070
Lev-iOS-Dev <159111910+Lev-iOS-Dev@users.noreply.github.com>
2024-03-06 01:46:51 +0400

    добавил логику для показа политики конфиденциальности

--- UIKitDZ/UIKitDZ/Application/Modules/ProfileModule/View/CardView.swift
+++ UIKitDZ/UIKitDZ/Application/Modules/ProfileModule/View/CardView.swift
@@ -3,6 +3,7 @@
 
 import UIKit
 
+/// Вью для отображения политики конфиденциальности
 final class CardView: UIView {
     // MARK: - Constants
 
@@ -42,6 +43,29 @@ final class CardView: UIView {
 
     // MARK: - Visual Components
 
+    private let panView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .panView
+        view.layer.cornerRadius = 5 / 2
+        return view
+    }()
+
+    private lazy var xButton: UIButton = {
+        let button = UIButton()
+        button.contentMode = .scaleAspectFill
+        button.tintColor = .black
+        button.setImage(
+            UIImage(systemName: Constants.xButtonImageName),
+            for: .normal
+        )
+        button.addTarget(
+            self,
+            action: #selector(cancelTapped),
+            for: .touchUpInside
+        )
+        return button
+    }()
+
     private lazy var mainLabel: UILabel = {
         let label = UILabel()
         label.font = .systemFont(ofSize: 20)
@@ -54,38 +78,23 @@ final class CardView: UIView {
         textView.textColor = .black
         textView.textAlignment = .left
         textView.showsVerticalScrollIndicator = false
+        textView.isScrollEnabled = false
         textView.isEditable = false
         textView.font = .systemFont(ofSize: 14)
+        textView.backgroundColor = .lightGray
         return textView
     }()
-
-    private lazy var xButton: UIButton = {
-        let button = UIButton()
-        button.setImage(UIImage(systemName: Constants.xButtonImageName), for: .normal)
-        button.contentMode = .scaleAspectFill
-        button.tintColor = .black
-        button.addTarget(self, action: #selector(cancelTapped), for: .touchUpInside)
-        return button
-    }()
-
-    // MARK: - Private Properties
-
-    //        var presenter: TermsOfUsePresenterProtocol?
 
     // MARK: - Initializers
 
     override init(frame: CGRect) {
         super.init(frame: frame)
-        backgroundColor = .green
         setupSubviews()
-        setupMainLabelConstraints()
-        setupMainTextViewConstraints()
-        setupXbuttonConstraints()
+        configureSubviews()
         setLabelText(
             title: Constants.title,
             text: Constants.text
         )
-        isUserInteractionEnabled = true
     }
 
     @available(*, unavailable)
@@ -96,32 +105,99 @@ final class CardView: UIView {
     // MARK: - Private Methodes
 
     private func setupSubviews() {
-        addSubviews([mainLabel, mainTextView, xButton], prepareForAutolayout: true)
-    }
-
-    private func setupMainLabelConstraints() {
-        NSLayoutConstraint.activate([
-            mainLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 25),
-            mainLabel.topAnchor.constraint(equalTo: topAnchor, constant: 45),
-            mainLabel.heightAnchor.constraint(equalToConstant: 20)
+        addSubviews([
+            panView,
+            xButton,
+            mainLabel,
+            mainTextView
         ])
     }
 
-    private func setupMainTextViewConstraints() {
+    private func configureSubviews() {
+        setupPanViewConstraints()
+        setupXbuttonConstraints()
+        setupMainLabelConstraints()
+        setupMainTextViewConstraints()
+    }
+
+    enum Insets {
+        static let top: CGFloat = 15
+    }
+
+    private func setupPanViewConstraints() {
         NSLayoutConstraint.activate([
-            mainTextView.leadingAnchor.constraint(equalTo: mainLabel.leadingAnchor),
-            mainTextView.topAnchor.constraint(equalTo: mainLabel.bottomAnchor),
-            mainTextView.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -25),
-            mainTextView.bottomAnchor.constraint(equalTo: bottomAnchor)
+            panView.topAnchor.constraint(
+                equalTo: topAnchor,
+                constant: 15
+            ),
+            panView.centerXAnchor.constraint(
+                equalTo: centerXAnchor
+            ),
+            panView.widthAnchor.constraint(
+                equalToConstant: 50
+            ),
+            panView.heightAnchor.constraint(
+                equalToConstant: 5
+            )
         ])
     }
 
     private func setupXbuttonConstraints() {
         NSLayoutConstraint.activate([
-            xButton.topAnchor.constraint(equalTo: topAnchor, constant: 15),
-            xButton.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -10),
-            xButton.widthAnchor.constraint(equalToConstant: 24),
-            xButton.heightAnchor.constraint(equalToConstant: 24)
+            xButton.topAnchor.constraint(
+                equalTo: topAnchor,
+                constant: 15
+            ),
+            xButton.trailingAnchor.constraint(
+                equalTo: trailingAnchor,
+                constant: -10
+            ),
+            xButton.leadingAnchor.constraint(
+                equalTo: leadingAnchor,
+                constant: 350
+            ),
+            xButton.widthAnchor.constraint(
+                equalToConstant: 24
+            ),
+            xButton.heightAnchor.constraint(
+                equalToConstant: 24
+            )
+        ])
+    }
+
+    private func setupMainLabelConstraints() {
+        NSLayoutConstraint.activate([
+            mainLabel.topAnchor.constraint(
+                equalTo: topAnchor,
+                constant: 45
+            ),
+            mainLabel.leadingAnchor.constraint(
+                equalTo: leadingAnchor,
+                constant: 25
+            ),
+            mainLabel.heightAnchor.constraint(
+                equalToConstant: 20
+            )
+        ])
+    }
+
+    private func setupMainTextViewConstraints() {
+        NSLayoutConstraint.activate([
+            mainTextView.topAnchor.constraint(
+                equalTo: mainLabel.bottomAnchor,
+                constant: 5
+            ),
+            mainTextView.leadingAnchor.constraint(
+                equalTo: leadingAnchor,
+                constant: 25
+            ),
+            mainTextView.centerXAnchor.constraint(
+                equalTo: centerXAnchor
+            ),
+            mainTextView.heightAnchor.constraint(
+                equalTo: heightAnchor,
+                constant: -150
+            )
         ])
     }
 
